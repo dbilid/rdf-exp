5d8c7008f4ee61a1cd0596c57332bc2ee6669066
Dimitris Bilidas <dbilid@pyravlos5>
2019-02-01 12:54:33 +0200

    using sqlite as storage

--- utilities/NewFastEncoder/src/partitioner_store.cpp
+++ utilities/NewFastEncoder/src/partitioner_store.cpp
@@ -9,6 +9,8 @@
 #include <sys/types.h>
 #include <dirent.h>
 #include <errno.h>
+#include "sqlite3.h"
+#include <sstream>
 
 int getdir (string dir, vector<string> &files)
 {
@@ -40,8 +42,8 @@ void partitioner_store::load_encode_rdf_data(string input_dir, string output_fil
 	Profiler profiler;
 	profiler.startTimer("load_rdf_data");
 	boost::unordered_map<string, ll>::iterator map_it;
-	ll so_id = 0;
-	ll predicate_id = 0;
+	ll so_id = 1;
+	ll predicate_id = 1;
 	ll num_rec = 0;
 	triple tmp_triple;
 	ofstream ofs;
@@ -51,6 +53,35 @@ void partitioner_store::load_encode_rdf_data(string input_dir, string output_fil
 	vector<triple> tmp_data;
 	string subject,predicate,object,objectSubType;
 	vector<string> files = vector<string>();
+	
+	inserts=vector<sqlite3_stmt*>();
+
+
+
+	int rc;
+        sqlite3 *dbExt;
+        string sql;
+        rc = sqlite3_open_v2("file::memory:", &dbExt,
+        SQLITE_OPEN_READWRITE | SQLITE_OPEN_URI | SQLITE_OPEN_CREATE, NULL);
+        if (rc != SQLITE_OK) {
+
+                fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(dbExt));
+                //sqlite3_close(db);
+
+                return ;
+        }
+        sql= string("attach database '")+string("/tmp/rdf.db")+string("' as ext;");
+        //strcpy(sql, "select * from  p0all;");
+        rc = sqlite3_exec(dbExt, sql.c_str(), 0, 0, NULL);
+
+        if (rc != SQLITE_OK) {
+
+                fprintf(stderr, "Cannot attach database: %s\n", sqlite3_errmsg(dbExt));
+                //sqlite3_close(db);
+
+                return ;
+        }
+
 
 	getdir(input_dir,files);
 	ofs.open(output_file_name.c_str(), ofstream::out);
@@ -88,6 +119,8 @@ void partitioner_store::load_encode_rdf_data(string input_dir, string output_fil
 					predicate_map[predicate] = predicate_id;
 					pid = predicate_id;
 					predicate_id++;
+					createNextPropertyTable(dbExt) ;
+					
 				}
 				else{
 					pid = map_it->second;
@@ -100,11 +133,11 @@ void partitioner_store::load_encode_rdf_data(string input_dir, string output_fil
 				}
 				map_it = so_map.find(object);
 				if (map_it == so_map.end()) {
-					if(objectType == 1){
-						so_map["\""+object+"\""] = so_id;
-					}
-					else
-						so_map[object] = so_id;
+					//if(objectType == 1){
+					//	so_map["\""+object+"\""] = so_id;
+					//}
+					//else
+					so_map[object] = so_id;
 					oid = so_id;
 					so_id++;
 				}
@@ -137,7 +170,78 @@ void partitioner_store::load_encode_rdf_data(string input_dir, string output_fil
 	profiler.pauseTimer("load_rdf_data");
 	print_to_screen("Done with data encoding in " + toString(profiler.readPeriod("load_rdf_data")) + " sec");
 	profiler.clearTimer("load_rdf_data");
-	this->dump_dictionaries(output_file_name);
+	//this->dump_dictionaries(output_file_name);
+
+
+	/*int rc;
+	sqlite3 *dbExt;
+	string sql;
+	rc = sqlite3_open_v2("file::memory:", &dbExt,
+	SQLITE_OPEN_READWRITE | SQLITE_OPEN_URI | SQLITE_OPEN_CREATE, NULL);
+	if (rc != SQLITE_OK) {
+
+		fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(dbExt));
+		//sqlite3_close(db);
+
+		return ;
+	}
+	sql= string("attach database '")+string("/tmp/rdf.db")+string("' as ext;");
+	//strcpy(sql, "select * from  p0all;");
+	rc = sqlite3_exec(dbExt, sql.c_str(), 0, 0, NULL);
+
+	if (rc != SQLITE_OK) {
+
+		fprintf(stderr, "Cannot attach database: %s\n", sqlite3_errmsg(dbExt));
+		//sqlite3_close(db);
+
+		return ;
+	}
+*/
+	sql= "PRAGMA page_size = 4096;";
+
+	sqlite3_exec(dbExt, sql.c_str(), 0, 0, NULL);
+
+	sql= "PRAGMA cache_size = 1048576;";
+
+	sqlite3_exec(dbExt, sql.c_str(), 0, 0, NULL);
+
+	sql= "PRAGMA synchronous = OFF;";
+
+	sqlite3_exec(dbExt, sql.c_str(), 0, 0, NULL);
+	sql= "PRAGMA locking_mode = EXCLUSIVE;";
+
+	sqlite3_exec(dbExt, sql.c_str(), 0, 0, NULL);
+	sql= "PRAGMA journal_mode = OFF;";
+
+	sqlite3_exec(dbExt, sql.c_str(), 0, 0, NULL);
+	sql= "PRAGMA temp_store=2;";
+	sqlite3_exec(dbExt, sql.c_str(), 0, 0, NULL);
+	//createMemoryDB(dbt);
+	//strcpy(sql, "detach database aa;");
+	//sqlite3_exec(dbt, sql, 0, 0, NULL);
+	//strcpy(sql, "PRAGMA query_only = 1;");
+	sqlite3_exec(dbExt, "BEGIN", 0, 0, 0);
+	saveDictionaryToDisk(so_map, dbExt, 1);
+	saveDictionaryToDisk(predicate_map, dbExt, 0);
+	rc = sqlite3_exec(dbExt, "END", 0, 0, 0);
+	if (rc != SQLITE_OK) {
+		
+		//fprintf(stderr, "Cannot commit: %s\n", sqlite3_errmsg(dbExt));
+		sqlite3_close(dbExt);
+		cout << "Error(" << rc << ") closing connection ";
+		return ;
+	}
+
+	rc = sqlite3_close(dbExt);
+	if (rc != SQLITE_OK) {
+
+		//fprintf(stderr, "Cannot close ext connection: %s\n",
+		//		sqlite3_errmsg(dbExt));
+		sqlite3_close(dbExt);
+		cout << "Error(" << rc << ") closing connection ";
+		return;
+	}
+	
 }
 
 void partitioner_store::dump_dictionaries(string file_name) {
@@ -165,4 +269,121 @@ void partitioner_store::dump_encoded_data(ofstream &output_stream, vector<triple
 	profiler.pauseTimer("dump_encoded_data");
 	print_to_screen("Done with dump_encoded_data in "+toString(profiler.readPeriod("dump_encoded_data"))+" sec");
 	profiler.clearTimer("dump_encoded_data");
+}
+
+
+void partitioner_store::saveDictionaryToDisk(boost::unordered_map<string, ll> my_map, sqlite3 *db, int dictionary) {
+	
+	string sql;
+	if (dictionary) {
+		sql="create table ext.dictionary(id INTEGER PRIMARY KEY, uri TEXT)";
+	} else {
+		sql="create table ext.properties(id INTEGER PRIMARY KEY, uri TEXT)";
+	}
+	int rc;
+	rc = sqlite3_exec(db, sql.c_str(), 0, 0, NULL);
+	if (rc != SQLITE_OK) {
+		cout << "Error(" << rc << ") saving dictionary ";
+		//fprintf(stderr, "could not create dictionary : %s\n",
+		//sqlite3_errmsg(db));
+		return;
+	}
+	if (dictionary) {
+		sql= "insert into ext.dictionary values(?, ?)";
+	} else {
+		sql= "insert into ext.properties values(?, ?)";
+	}
+
+	sqlite3_stmt *res;
+	rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &res, 0);
+	if (rc != SQLITE_OK) {
+		cout << "Error(" << rc << ") saving dictionary ";
+		//fprintf(stderr, "Cannot prepare statement: %s\n", sqlite3_errmsg(db));
+		return;
+	}
+
+	//GHashTableIter iter;
+	//gpointer key, value;
+
+	//g_hash_table_iter_init(&iter, hash_table);
+
+	typename boost::unordered_map<string,unsigned int>::iterator i;
+	string uri;
+	ll id;
+	//inverse=1;
+        for(i = my_map.begin(); i != my_map.end(); i++){
+          //      if(inverse){
+	//		uri=toString((*i).second);
+	//		id=(*i).first;
+                        //mapStream<<toString((*i).second)<<" "<<toString((*i).first)<<endl;
+          //      }
+            //    else{
+			uri=toString((*i).first);
+                        id=(*i).second;
+
+                        //mapStream<<toString((*i).first)<<" "<<toString((*i).second)<<endl;
+             //  }
+	addBatch(uri.c_str(), id, res);
+        }
+//        mapStream.close();
+	//while (g_hash_table_iter_next(&iter, &key, &value)) {
+		//printf("key:%p values:%p \n", key, value);
+	//	char* k = (char*) key;
+	//	int v = GPOINTER_TO_INT(value);
+		//addBatch(uri.c_str(), id, res);
+	//}
+
+	rc = sqlite3_finalize(res);
+	//free(sql);
+
+}
+
+void partitioner_store::addBatch(char const* key, ll value, sqlite3_stmt *res) {
+	//printf("key:%s value:%d \n", key, value);
+	sqlite3_bind_int(res, 1, value);
+	sqlite3_bind_text(res, 2, key, -1, SQLITE_STATIC);
+	if (sqlite3_step(res) != SQLITE_DONE) {
+		printf("Commit Failed!\n");
+	}
+	sqlite3_reset(res);
+}
+
+
+void partitioner_store::createNextPropertyTable(sqlite3 *dbExt) {
+	
+	//string create=string("create table ext.prop")+boost::lexical_cast<std::string>(inserts.size()+1)+string(" (s INTEGER, o INTEGER, primary key(s, o)) without rowid;");
+	std::ostringstream create;
+	create << "create table ext.prop" << (inserts.size()+1) << " (s INTEGER, o INTEGER, primary key(s, o)) without rowid;";
+	//char create[100];
+	//snprintf(create, sizeof create, "%s%i%s", createPrefix, propId,
+	//		createPostfix);
+	int rc;
+	rc = sqlite3_exec(dbExt, create.str().c_str(), 0, 0, NULL);
+
+	if (rc != SQLITE_OK) {
+
+		fprintf(stderr, "Could not create property table: %s\n",
+		sqlite3_errmsg(dbExt));
+		//sqlite3_close(dbExt);
+
+		return;
+	}
+	//char insert[100];
+	//string insert;
+	std::ostringstream insert;
+        insert << "insert or ignore into ext.prop" << (inserts.size()+1) << " values(?, ?) ;";
+	//insert=string("insert or ignore into ext.prop"+boost::lexical_cast<std::string>(inserts.size()+1)+string(" values(?, ?) ;");
+	//snprintf(insert, sizeof insert, "%s%i%s", insertPrefix, propId,
+	//		insertPostfix);
+	sqlite3_stmt *stmt; 
+	rc = sqlite3_prepare_v2(dbExt, insert.str().c_str(), -1, &stmt, 0);
+	if (rc != SQLITE_OK) {
+
+		fprintf(stderr, "Cannot prepare statement: %s\n",
+		sqlite3_errmsg(dbExt));
+		//sqlite3_close(dbExt);
+
+		return;
+	}
+	inserts.push_back(stmt);
 }
--- utilities/NewFastEncoder/src/partitioner_store.h
+++ utilities/NewFastEncoder/src/partitioner_store.h
@@ -11,6 +11,7 @@
 #include "profiler.h"
 #include "row.h"
 #include "TurtleParser.hpp"
+#include "sqlite3.h"
 using namespace std;
 
 struct triple{
@@ -26,12 +27,12 @@ struct triple{
 	ll object;
 	Type::ID ot;
 	string print(){
-		if(ot == 1){
-			return "<"+toString(subject)+"> <"+toString(predicate)+"> \""+toString(object)+"\" .";
-		}
-		else{
-			return "<"+toString(subject)+"> <"+toString(predicate)+"> <"+toString(object)+"> .";
-		}
+		//if(ot == 1){
+		//	return "<"+toString(subject)+"> <"+toString(predicate)+"> \""+toString(object)+"\" .";
+		//}
+		//else{
+			return toString(subject)+" "+toString(predicate)+" "+toString(object);
+		//}
 	}
 };
 
@@ -41,11 +42,15 @@ public:
 	virtual ~partitioner_store();
 	void load_encode_rdf_data(string input_dir, string output_file_name);
 	boost::unordered_map<string, ll> so_map;
+	vector<sqlite3_stmt*> inserts;
 	boost::unordered_map<string, ll> predicate_map;
 	void dump_encoded_data(ofstream &output_stream, vector<triple> & data);
 	ll total_data_size;
 protected:
 	void dump_dictionaries(string file_name);
+	void addBatch(char const* key,ll value, sqlite3_stmt *res);
+	void saveDictionaryToDisk(boost::unordered_map<string,ll> my_map, sqlite3 *db, int dictionary);
+	void createNextPropertyTable(sqlite3 *dbExt) ;
 };
 
 #endif /* DATASTORE_H_ */
